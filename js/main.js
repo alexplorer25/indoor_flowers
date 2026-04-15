079f808ab8c390cda49001acc6d683c8f665fc56
Alexandr Romanuk <alexplorer@alexplorer.ru>
2026-04-15 19:30:05 +0300

    14-04-26

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel();
-});
-
-$(".owl-carousel").owlCarousel({
+$(".slider__featured").owlCarousel({
   //   responsiveClass: true,
   //   responsive: {
   //     // от 0px и больше
@@ -23,6 +19,33 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   items: 4,
   margin: 68,
+  autoplay: false,
+  autoplayTimeout: 5000,
+  dotsEach: true,
+  autoWidth: false,
+});
+
+$(".slider__plant").owlCarousel({
+  //   responsiveClass: true,
+  //   responsive: {
+  //     // от 0px и больше
+  //     0: {
+  //       items: 2,
+  //     },
+  //     // от 768px
+  //     768: {
+  //       items: 3,
+  //     },
+  //     // от 1190px
+  //     1184: {
+  //       items: 6,
+  //     },
+  //   },
+  nav: true,
+  dots: false,
+  loop: true,
+  items: 3,
+  margin: 25,
   autoplay: false,
   autoplayTimeout: 5000,
   dotsEach: true,
